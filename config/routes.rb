1ddda5245e01d7a1bd0c4deb0d08eadfb2179f45
musomoka <musosum@gmail.com>
2017-01-10 17:16:29 +0000

    master commit chapter 3 end

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+root 'static_pages#Home'
+  
+  get 'static_pages/Home'
+
+  get 'static_pages/Help'
+  
+  get 'static_pages/About'
+  
+ 
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
